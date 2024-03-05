725a85551113dd5323165dd1ed16f3459a3cf7ad
ricardoaugusto <contato@ricardoaugusto.com>
2024-03-05 23:05:32 +0000

    user crud
    Signed-off-by: ricardoaugusto <contato@ricardoaugusto.com>

--- router/user.py
+++ router/user.py
@@ -1,7 +1,10 @@
-from schemas.user import UserBase, UserDisplay
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+
 from db.database import get_db
+from schemas.user import UserBase, UserDisplay
 from services import user
 
 router = APIRouter(
@@ -15,8 +18,22 @@ router = APIRouter(
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return user.create_user(db, request)
 
-# Read user
 
-# Update user
+@router.get('/', response_model=List[UserDisplay])
+def get_users(db: Session = Depends(get_db)):
+    return user.get_users(db)
 
-# Delete user
+
+@router.get('/{id}', response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return user.get_user(db, id)
+
+
+@router.put('/{id}')
+def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+    return user.update_user(db, id, request)
+
+
+@router.delete('/{id}')
+def delete_user(id: int, db: Session = Depends(get_db)):
+    return user.delete_user(db, id)
--- services/user.py
+++ services/user.py
@@ -1,7 +1,8 @@
-from db.hash import Hash
 from sqlalchemy.orm.session import Session
-from schemas.user import UserBase
+
+from db.hash import Hash
 from db.models import DbUser
+from schemas.user import UserBase
 
 """
 User service handles the creation of a new user in the database.
@@ -18,3 +19,29 @@ def create_user(db: Session, request: UserBase):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+def get_users(db: Session):
+    return db.query(DbUser).all()
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(DbUser).filter(DbUser.id == user_id).first()
+
+
+def update_user(db: Session, id: int, request: UserBase):
+    user = db.query(DbUser).filter(DbUser.id == id)
+    user.update({
+        DbUser.username: request.username,
+        DbUser.email: request.email,
+        DbUser.password: Hash.bcrypt(request.password)
+    })
+    db.commit()
+    return 'ok'
+
+
+def delete_user(db: Session, id: int):
+    user = db.query(DbUser).filter(DbUser.id == id).first()
+    db.delete(user)
+    db.commit()
+    return 'ok'
